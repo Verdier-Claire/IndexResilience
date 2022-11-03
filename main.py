81966c3318f33ba381a3bf4393f11816a67ec081
Verdier-Claire <claire.verdier@edu.univ-fcomte.fr>
2022-11-03 14:09:51 +0100

    main to compute all of three index in the same time

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from AgilityCompanyProduction import AgilityCompanyProduct
+from LocalBackupSupplier import LocalBackupSuppliers
+from versatilityCompanyWorkforce import Versatility
+
+Versa = Versatility()
+lbs = LocalBackupSuppliers()
+acp = AgilityCompanyProduct()
+
+if __name__ == '__main__':
+    data_versatility = Versa.main_vcw()
+    data_agitility = acp.main_acp()
+    data_localbackup = lbs.main_lbs()
+
+    data_final = data_agitility.merge(data_versatility, on=['code'])
+    data_final = data_final.merge(data_localbackup, on=['code'])
+    data_final.to_csv(os.getcwd() + "/data/data_out/indexResilient.csv", sep=';')
