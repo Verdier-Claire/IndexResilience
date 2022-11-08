66624195316d93c5d41ffe3bbdc592aad81e42d4
Verdier-Claire <claire.verdier@edu.univ-fcomte.fr>
2022-11-08 10:15:43 +0100

    name file change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from AgilityCompanyProduction import AgilityCompanyProduct
 from LocalBackupSupplier import LocalBackupSuppliers
-from versatilityCompanyWorkforce import Versatility
+from VersatilityCompanyWorkforce import Versatility
 
 Versa = Versatility()
 lbs = LocalBackupSuppliers()
